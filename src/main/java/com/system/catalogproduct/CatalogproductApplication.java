3082e4c1235352c42a6a8adc1601e5e8d55332a7
ferfcb99 <fer_fcb99@outlook.com>
2025-08-17 21:04:05 -0600

    New project catalog microservice created

---
+++ src/main/java/com/system/catalogproduct/CatalogproductApplication.java
@@ -0,0 +1,13 @@
+package com.system.catalogproduct;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CatalogproductApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CatalogproductApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/system/catalogproduct/entity/Category.java
@@ -0,0 +1,47 @@
+package com.system.catalogproduct.entity;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "category")
+public class Category {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Long id;
+
+    @Column(name = "name", nullable = false, unique = true)
+    private String name;
+
+    public Category() {}
+
+    public Category(Long id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        return "Category{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                '}';
+    }
+}
